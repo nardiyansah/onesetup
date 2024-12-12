f5d155daf3b27b8f157cc6f33b9574103b557605
nardiyansahtj <nardiyansah.jatmiko@bytedance.com>
2024-12-12 07:54:12 +0700

    docs(todo): write todo step in script

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
 # This file for install the setup for new lepi
+
+# made backup for dotfiles
+if [ -d "$HOME/backup" ]; then
+    echo "baclu"
+fi
+# Homebrew
+
+# iTerm2
+
+# set zsh as default shell
+
+# setup oh-my-zsh
+
+# git
+
+# setup vim
